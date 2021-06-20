35b41309b4844d442a0177e28d5b782ec8f27734
night5415 <meloymike@gmail.com>
2021-06-20 09:42:34 -0500

    added service worker

---
+++ sw.js
@@ -0,0 +1,42 @@
+var CACHE_NAME = "offline-fun-v1";
+var urlsToCache = ["/", "/styles/index.css", "/script/index.js"];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then(function (response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
